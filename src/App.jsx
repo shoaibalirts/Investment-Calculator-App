3cb2f5969c0ebe029c465b944c774ba673534e71
shoaibalirts <shoaibali.rts@gmail.com>
2024-07-24 09:17:08 +0200

    Calculated Results are derived from the user input state

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,31 @@
 import Header from "./components/Header";
+import Results from "./components/Results";
 import UserInput from "./components/UserInput";
+import { useState } from "react";
 
 function App() {
+  const [userInput, setUserInput] = useState({
+    initialInvestment: 10000,
+    annualInvestment: 1200,
+    expectedReturn: 6,
+    duration: 10,
+  });
+  function handleChange(inputIdentifier, newValue) {
+    setUserInput((prevUserInput) => {
+      return {
+        ...prevUserInput,
+        [inputIdentifier]: +newValue, // javascript dynamic value for inputIdentified
+      };
+    });
+  }
+  // Now, userInput state data will be calculated again using logic(or formula) given in investment.js
+  // and then it will be carried and forward to Results component for display table
+
   return (
     <>
       <Header />
-      <UserInput />
+      <UserInput onChange={handleChange} userInput={userInput} />
+      <Results input={userInput} />
     </>
   );
 }
